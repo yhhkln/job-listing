8b8fa0c36987e4e43c5230f8c88eec3613f93d3a
yhk <yhhklndexnug@gmail.com>
2017-04-24 02:52:08 +0800

    login as admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-
+before_action :configure_permitted_parameters, if: :devise_controller?
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = 'you are not admin'
       redirect_to root_path
     end
   end
-  
+  protected
+   def configure_permitted_parameters
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:is_admin])
+   end
+
 end
