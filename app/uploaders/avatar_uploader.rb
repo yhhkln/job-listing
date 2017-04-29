f167aa7d16315491a601c2b3e0c6feb59aac37fe
yhk <yhhklndexnug@gmail.com>
2017-04-29 23:45:14 +0800

    a

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,7 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  #storage :file
   storage :qiniu
 
   # Override the directory where uploaded files will be stored.
