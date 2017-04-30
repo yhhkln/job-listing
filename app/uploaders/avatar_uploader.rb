7312f97a83d178b34051a83cc13370c4ed3cc817
yhk <yhhklndexnug@gmail.com>
2017-04-30 19:18:54 +0800

    “xx”

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,8 +5,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file   #本地文件
-  #storage :qiniu #七牛云
+  #storage :file  #本地文件
+  storage :qiniu  #七牛云
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
