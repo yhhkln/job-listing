d7186d447725303d4c4edecd1dc2fb317060252b
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-05-05 23:56:45 +0800

    sq3

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,7 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :require_is_admin
-  before_action :find_job_and_check_permission, only: [:edit, :update, :destroy]
+
   layout "admin"
   def show
     @job = Job.find(params[:id])
